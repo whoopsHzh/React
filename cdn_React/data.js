f4a2cd21b39fe57366d05f9a94c8cd9031d33798
whoopsHzh <whoopshzh@gmail.com>
2019-12-11 11:01:37 +0800

    commit gh

---
+++ cdn_React/data.js
@@ -0,0 +1,66 @@
+data = [{
+        url: 'https://a-ssl.duitang.com/uploads/item/201905/08/20190508202050_iecsU.thumb.700_0.jpeg',
+        content: '你奏凯！！！！！！',
+        comment: [{
+                avator: 'https://c-ssl.duitang.com/uploads/item/201906/17/20190617103524_yxzwV.thumb.700_0.jpeg',
+                user: '毛九',
+                content: '好萌！！！！'
+            },
+            {
+                avator: 'https://c-ssl.duitang.com/uploads/item/201906/17/20190617103609_5NYHG.thumb.700_0.jpeg',
+                user: '好鸭好鸭',
+                content: '好可爱！！！！'
+            }
+        ]
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201905/15/20190515232319_XSmQS.thumb.700_0.jpeg',
+        content: '不敢看...',
+        comment: []
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201901/30/20190130052011_npvqd.thumb.700_0.jpeg',
+        content: '小龙妈',
+        comment: []
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201906/13/20190613194116_lliqi.thumb.700_0.jpeg',
+        content: '帅的一批',
+        comment: []
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201906/13/20190613194212_mdfmx.thumb.700_0.jpeg',
+        content: 'sunshine',
+        comment: [{
+                avator: 'https://c-ssl.duitang.com/uploads/item/201907/15/20190715222710_cuzxl.thumb.700_0.jpg',
+                user: '织世',
+                content: '收集到  我和星河都甘做你的陪衬'
+            },
+            {
+                avator: 'https://c-ssl.duitang.com/uploads/item/201907/10/20190710215420_ljdzi.thumb.700_0.jpeg',
+                user: '远远',
+                content: '阳光美丽'
+            }
+        ]
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201906/27/20190627190022_KNGi3.thumb.700_0.jpeg',
+        content: '动漫头像٩(˃̶͈̀௰˂̶͈́)و小鹿搬运',
+        comment: []
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201905/12/20190512193253_vHKLu.thumb.700_0.jpeg',
+        content: 'GALeeの自截',
+        comment: []
+    },
+    {
+        url: 'https://c-ssl.duitang.com/uploads/item/201906/17/20190617103603_kLaci.thumb.700_0.jpeg',
+        content: '动漫头像',
+        comment: [{
+            avator: 'https://c-ssl.duitang.com/uploads/item/201906/07/20190607162548_bvlif.thumb.700_0.jpg',
+            user: '我怎么会这么帅',
+            content: '已收藏'
+        }]
+    }
+
+]
